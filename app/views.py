67a0fa42db754d861f5bff8c155157e76d8c4c8a
Your Name <you@example.com>
2023-06-01 21:17:53 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bhavya(request):
+    return render(request,'bhavya.html')
+
+
+
+def ankitha(request):
+    return render(request,'ankitha.html')
